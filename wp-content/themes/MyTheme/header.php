e121b51ffad15abb88ccb9fb4c5f11609f303b7e
casxai <joeynicole99@gmail.com>
2024-04-26 13:28:27 +0800

    fix footer

--- wp-content/themes/MyTheme/header.php
+++ wp-content/themes/MyTheme/header.php
@@ -18,21 +18,22 @@
                 <a class="navbar-brand fs-4" href="#">PORTFOLIO</a>
 
                 <!-- Toggle button -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                <button class="navbar-toggler navbar-toggler-header" type="button" data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
                     aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <!-- Menu -->
-                <div class="collapse navbar-collapse justify-content-end " id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse navbar-collapse-header justify-content-end "
+                    id="bs-example-navbar-collapse-1">
                     <?php
                     wp_nav_menu(
                         array(
                             'theme_location' => 'top-menu',
                             'depth' => 2,
                             'container' => false,
-                            'menu_class' => 'navbar-nav hide-mobile-menu m-0 p-0',
+                            'menu_class' => 'navbar-nav navbar-nav-header m-0 p-0',
                             'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
                             'walker' => new WP_Bootstrap_Navwalker(),
                         )
